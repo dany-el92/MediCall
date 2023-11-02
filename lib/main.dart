5cc387e6196f395a6278bc676a65579b6bdef18d
Daniele <d.gregori1@studenti.unisa.it>
2023-11-02 23:52:56 +0100

    Creazione email e password textfield

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() {
   runApp(MyApp());
 }
 
-// test
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -3,8 +3,16 @@ import 'package:medicall/utilities/extensions.dart';
 
 import '../../constants/images.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isObscure = true;
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +31,99 @@ class LoginScreen extends StatelessWidget {
                 Color(0XFF002556),
                 Color(0XFF00204A)
               ])),
-          child: Column(
-            children: [
-              Image.asset(ImageConstant.imgLogo),
-
-            ],
+          child: Form(
+            child: Column(
+              children: [
+                Image.asset(ImageConstant.imgLogo),
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.emailAddress,
+                        enableSuggestions: true,
+                        decoration: InputDecoration(
+                          //label
+                          labelText: 'Email',
+                          labelStyle: const TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          suffixIcon: const Icon(Icons.email),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          fillColor: Colors.white,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 2),
+                          ),
+                        ),
+                        onTapOutside: (event) =>
+                            FocusScope.of(context).unfocus(),
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        textInputAction: TextInputAction.done,
+                        keyboardType: TextInputType.visiblePassword,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        obscureText: isObscure,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                          //label
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                isObscure = !isObscure;
+                              });
+                            },
+                            style: IconButton.styleFrom(
+                              minimumSize: const Size(48, 48),
+                            ),
+                            icon: Icon(
+                              isObscure
+                                  ? Icons.visibility_off_outlined
+                                  : Icons.visibility_outlined,
+                              color: Colors.black,
+                            ),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          fillColor: Colors.white,
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 2),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide:
+                                const BorderSide(color: Colors.black, width: 2),
+                          ),
+                        ),
+                        onTapOutside: (event) =>
+                            FocusScope.of(context).unfocus(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
