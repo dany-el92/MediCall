acff183636dd501e58a31b1f4c6bc01d316b6dae
Daniele <d.gregori1@studenti.unisa.it>
2023-11-15 17:31:18 +0100

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: Routes.splashScreen,
+        initialRoute: Routes.mainView,
         routes: {
           Routes.loginView: (context) => const LoginView(),
           Routes.registerView: (context) => const RegisterView(),
