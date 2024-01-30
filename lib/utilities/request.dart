dc338f475560afee3f2375337ff12f032491161d
Daniele <d.gregori1@studenti.unisa.it>
2024-01-30 18:03:45 +0100

    Implementazione dati prenotazione inviati al server dall'assistente flutter

--- lib/utilities/request.dart
+++ lib/utilities/request.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:medicall/constants/secret.dart';
+import 'package:medicall/utilities/regex_helper.dart';
 
 Future getData(url) async {
   var uri = Uri.parse(url);
@@ -18,7 +19,7 @@ class OpenAIService {
       "content": '''
      Dalla frase seguente, estrai le informazioni di ora, giorno, luogo e prestazione, seguendo questo template: 
       -Ora:
-      -Giorno:
+      -Data:
       -Luogo:
       -Prestazione:
     ''',
@@ -38,7 +39,7 @@ class OpenAIService {
           "model": "gpt-3.5-turbo",
           "messages": messages,
           "max_tokens": 100,
-          "temperature": 0.3,
+          "temperature": 0.1,
         }),
       );
 
@@ -51,11 +52,41 @@ class OpenAIService {
           'role': 'assistant',
           'content': content,
         });
+
+        await extractAndSendData(content);
+
         return content;
       }
       return 'An internal error occurred';
     } catch (e) {
       return e.toString();
+    }
+  }
+
+  Future<void> extractAndSendData(String content) async {
+    // Extract the information from the generated content
+    String ora = RegexHelper.extractOra(content);
+    String data = RegexHelper.extractData(content);
+    String luogo = RegexHelper.extractLuogo(content);
+    String prestazione = RegexHelper.extractPrestazione(content);
+
+    // Send the extracted information to the server
+    var response = await http.post(
+      Uri.parse('https://5949-95-251-24-61.ngrok-free.app/extract'),
+      // Replace with your server URL
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        'ora': ora,
+        'data': data,
+        'luogo': luogo,
+        'prestazione': prestazione,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('Information sent to the server successfully');
+    } else {
+      print('Failed to send information to the server');
     }
   }
 }
